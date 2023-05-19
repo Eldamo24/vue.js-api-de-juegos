aaca87af0c082f7c81f9918fedb4bedc8d5808e6
Damian Lobos <damianlobos24@gmail.com>
2023-05-19 18:02:38 -0300

    Problemas solucionados

--- app.js
+++ app.js
@@ -5,29 +5,17 @@ const myApp = {
         }
     },
      mounted() {
-        try{
-            this.traerJuegos().then(res => {
-                for(game of res.data.results){
-                    this.juegos.push(game)
-                }
-            })
-            console.log(this.juegos)
-        }catch(error){
-            console.log("Error fatal ", error)
-        }
-             
-            
+        this.getData()           
     },
     methods: {
-
-        async traerJuegos() {
-            try {
-                return await axios("https://api.rawg.io/api/games?key=aa2e1c5b1fdf46ada9240e4ab295ee9a")
-            } catch (error) {
-                console.log("Error fatal ", error)
-                return undefined
-            }
-        },
+        getData(){
+            fetch("https://api.rawg.io/api/games?key=aa2e1c5b1fdf46ada9240e4ab295ee9a")
+            .then(response => response.json())
+            .then(data => {
+                this.juegos = data
+            })
+            
+        }
 
     }
 }
